b29ce98c7059db4f3ffacb66e225b062af79d4f5
shekhar-kotekar <shekhar.kotekar@gmail.com>
2024-08-25 16:59:00 +0200

    docker ignore file added

--- core/src/main.rs
+++ core/src/main.rs
@@ -10,6 +10,7 @@ mod models;
 
 #[tokio::main]
 async fn main() {
+    common::enable_tracing();
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
         .await
         .unwrap();
